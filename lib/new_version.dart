02b31190c34f58c17ad732434df4038a2eda0d2f
Loi Tran <loit@ceterus.com>
2021-08-23 09:59:20 +0700

    make it compatible with my project

--- lib/new_version.dart
+++ lib/new_version.dart
@@ -25,7 +25,7 @@ class VersionStatus {
   final String appStoreLink;
 
   /// The release notes for the store version of the app.
-  final String? releaseNotes;
+  final String releaseNotes;
 
   /// True if the there is a more recent version of the app in the store.
   // bool get canUpdate => localVersion.compareTo(storeVersion).isNegative;
@@ -53,9 +53,9 @@ class VersionStatus {
   }
 
   VersionStatus._({
-    required this.localVersion,
-    required this.storeVersion,
-    required this.appStoreLink,
+    this.localVersion,
+    this.storeVersion,
+    this.appStoreLink,
     this.releaseNotes,
   });
 }
@@ -64,18 +64,18 @@ class NewVersion {
   /// An optional value that can override the default packageName when
   /// attempting to reach the Apple App Store. This is useful if your app has
   /// a different package name in the App Store.
-  final String? iOSId;
+  final String iOSId;
 
   /// An optional value that can override the default packageName when
   /// attempting to reach the Google Play Store. This is useful if your app has
   /// a different package name in the Play Store.
-  final String? androidId;
+  final String androidId;
 
   /// Only affects iOS App Store lookup: The two-letter country code for the store you want to search.
   /// Provide a value here if your app is only available outside the US.
   /// For example: US. The default is US.
   /// See http://en.wikipedia.org/wiki/ ISO_3166-1_alpha-2 for a list of ISO Country Codes.
-  final String? iOSAppStoreCountry;
+  final String iOSAppStoreCountry;
 
   NewVersion({
     this.androidId,
@@ -85,8 +85,8 @@ class NewVersion {
 
   /// This checks the version status, then displays a platform-specific alert
   /// with buttons to dismiss the update alert, or go to the app store.
-  showAlertIfNecessary({required BuildContext context}) async {
-    final VersionStatus? versionStatus = await getVersionStatus();
+  showAlertIfNecessary({BuildContext context}) async {
+    final VersionStatus versionStatus = await getVersionStatus();
     if (versionStatus != null && versionStatus.canUpdate) {
       showUpdateDialog(context: context, versionStatus: versionStatus);
     }
@@ -95,7 +95,7 @@ class NewVersion {
   /// This checks the version status and returns the information. This is useful
   /// if you want to display a custom alert, or use the information in a different
   /// way.
-  Future<VersionStatus?> getVersionStatus() async {
+  Future<VersionStatus> getVersionStatus() async {
     PackageInfo packageInfo = await PackageInfo.fromPlatform();
     if (Platform.isIOS) {
       return _getiOSStoreVersion(packageInfo);
@@ -109,11 +109,11 @@ class NewVersion {
 
   /// iOS info is fetched by using the iTunes lookup API, which returns a
   /// JSON document.
-  Future<VersionStatus?> _getiOSStoreVersion(PackageInfo packageInfo) async {
+  Future<VersionStatus> _getiOSStoreVersion(PackageInfo packageInfo) async {
     final id = iOSId ?? packageInfo.packageName;
     final parameters = {"bundleId": "$id"};
     if (iOSAppStoreCountry != null) {
-      parameters.addAll({"country": iOSAppStoreCountry!});
+      parameters.addAll({"country": iOSAppStoreCountry});
     }
     var uri = Uri.https("itunes.apple.com", "/lookup", parameters);
     final response = await http.get(uri);
@@ -131,7 +131,7 @@ class NewVersion {
   }
 
   /// Android info is fetched by parsing the html of the app store page.
-  Future<VersionStatus?> _getAndroidStoreVersion(
+  Future<VersionStatus> _getAndroidStoreVersion(
       PackageInfo packageInfo) async {
     final id = androidId ?? packageInfo.packageName;
     final uri =
@@ -145,13 +145,13 @@ class NewVersion {
 
     final additionalInfoElements = document.getElementsByClassName('hAyfc');
     final versionElement = additionalInfoElements.firstWhere(
-      (elm) => elm.querySelector('.BgcNfc')!.text == 'Current Version',
+      (elm) => elm.querySelector('.BgcNfc').text == 'Current Version',
     );
-    final storeVersion = versionElement.querySelector('.htlgb')!.text;
+    final storeVersion = versionElement.querySelector('.htlgb').text;
 
     final sectionElements = document.getElementsByClassName('W4P4ne');
     final releaseNotesElement = sectionElements.firstWhereOrNull(
-      (elm) => elm.querySelector('.wSaTQd')!.text == 'What\'s New',
+      (elm) => elm.querySelector('.wSaTQd').text == 'What\'s New',
     );
     final releaseNotes = releaseNotesElement
         ?.querySelector('.PHBdkd')
@@ -173,14 +173,14 @@ class NewVersion {
   /// optionally provide [dialogTitle], [dialogText], [updateButtonText],
   /// [dismissButtonText], and [dismissAction] parameters.
   void showUpdateDialog({
-    required BuildContext context,
-    required VersionStatus versionStatus,
+    BuildContext context,
+    VersionStatus versionStatus,
     String dialogTitle = 'Update Available',
-    String? dialogText,
+    String dialogText,
     String updateButtonText = 'Update',
     bool allowDismissal = true,
     String dismissButtonText = 'Maybe Later',
-    VoidCallback? dismissAction,
+    VoidCallback dismissAction,
   }) async {
     final dialogTitleWidget = Text(dialogTitle);
     final dialogTextWidget = Text(
